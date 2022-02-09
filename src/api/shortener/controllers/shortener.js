a33b72c8d12a79ef4851c908d1c73f7467f8f1ef
azhukov <azhukov@emphasoft.com>
2022-02-09 17:51:20 +0500

    added shortener controller

--- src/api/shortener/controllers/shortener.js
+++ src/api/shortener/controllers/shortener.js
@@ -6,4 +6,46 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::shortener.shortener');
+module.exports = createCoreController('api::shortener.shortener', ({strapi}) =>  ({
+  async find(ctx) {
+    let { query } = ctx;
+    const user = ctx.state.user;
+    let entity;
+    if (user) {
+      query = { user: { '$eq': user.id } }
+      entity = await strapi.service('api::shortener.shortener').find({ filters: query });
+    } else {
+      query = { alias: { '$eq': query.alias }}
+      entity = await strapi.service('api::shortener.shortener').find({ filters: query });
+      if (entity.results.length !== 0){
+        let id = entity.results[0].id;
+        let visit = Number(entity.results[0].visit) + 1;
+        await strapi.service('api::shortener.shortener').update(id, { data: { visit } });
+      }
+    }
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+  async create(ctx) {
+    const { data } = ctx.request.body;
+    const user = ctx.state.user;
+    let entity;
+    data.user = user.id;
+    entity = await strapi.service('api::shortener.shortener').create({ data });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+  async delete(ctx) {
+    let { id } = ctx.params;
+    const user = ctx.state.user;
+    let entity;
+    let query = { user: { '$eq': user.id }, id: { '$eq': id } };
+    entity = await strapi.service('api::shortener:shortener').find({ filters: query });
+    if (entity.results.length === 0){
+      return ctx.badRequest(null, [{ messages: [{ id: 'You can delete someone else content'}]}]);
+    }
+    entity = await strapi.service('api::shortener:shortener').delete(id);
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
